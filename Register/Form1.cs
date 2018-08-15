b763cae63a29c15068004d70e50e96e2c4dd3f7a
root <root@localhost.localdomain>
2018-08-14 21:14:07 -0700

    Register commit

---
+++ Register/Form1.cs
@@ -0,0 +1,141 @@
+//Miguel Pulido - Systems Architect
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            // Close this window
+            this.Close();
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/Clear.jpg");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/VOID.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/Open.jpg");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/9.jpg");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/8.jpg");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/7.jpg");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/6.jpg");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/5.jpg");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/4.jpg");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/3.jpg");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/2.jpg");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/1.jpg");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/0.jpg");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/00.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/ON.jpg");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/OFF.jpg");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/total.jpg");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/dot.jpg");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/check.jpg");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/credit.jpg");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/misc.jpg");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/Start.jpg");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("C:/KT_Project/Register/amount.jpg");
+        }
+    }
+}
